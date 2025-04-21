e70b14c366991727770d354ec68e374a42d07210
rokas <rokster112@gmail.com>
2025-04-21 22:04:38 +0300

    AuthRequired doesnt work as intended, adjusted if statement logic

--- src/components/AuthRequired.jsx
+++ src/components/AuthRequired.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom"
 
 export default function AuthRequired() {
   const loggedIn = JSON.parse(localStorage.getItem("user"))
-  if (!loggedIn.email) {
+  if (!loggedIn) {
     return (
       <Navigate
         to={"/login"}
