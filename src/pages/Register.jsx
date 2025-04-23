b3b5158b79fd1451c269d492a1b6910da1d6ab86
rokas <rokster112@gmail.com>
2025-04-23 18:36:25 +0300

    added another field in register - confirm password

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,20 +7,32 @@ export default function Register() {
   const [formData, setFormData] = React.useState({
     email: "",
     password: "",
+    confirmPassword: "",
   })
   const [err, setErr] = React.useState(null)
+  const [status, setStatus] = React.useState("idle")
   const navigate = useNavigate()
 
   function handleSubmit(e) {
     e.preventDefault()
+    setStatus("submitting")
     async function postData() {
       try {
+        if (formData.password !== formData.confirmPassword) {
+          setStatus("idle")
+          throw new Error("Passwords do not match, try again")
+        } else if (!formData.email || !formData.password) {
+          setStatus("idle")
+          throw new Error("Please fill in all fields")
+        }
         const data = await createUser(formData.email, formData.password)
         console.log("User created!", data.email)
         navigate("/login", { replace: true })
       } catch (error) {
         setErr(error)
         console.error(error.message)
+      } finally {
+        setStatus("idle")
       }
     }
     postData()
@@ -47,11 +59,23 @@ export default function Register() {
             className="register-input"
             type="password"
             name="password"
-            placeholder="current-password"
+            placeholder="create password"
             onChange={(e) => handleChange(e, setFormData, setErr)}
           />
         </label>
-        <button type="submit">Register</button>
+        <label className="register-label">
+          Confirm Password:
+          <input
+            className="register-input"
+            type="password"
+            name="confirmPassword"
+            placeholder="repeat password"
+            onChange={(e) => handleChange(e, setFormData, setErr)}
+          />
+        </label>
+        <button disabled={status === "submitting"} type="submit">
+          {status === "submitting" ? "Registering..." : "Register"}
+        </button>
       </form>
       <p className="register-p">
         Already have an account?
